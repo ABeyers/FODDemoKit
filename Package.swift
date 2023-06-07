74b4a08d151870b1c66d7d9aa30013934f1b2c7d
Amanda Beyers <amandawbeyers@gmail.com>
2023-06-07 15:00:45 -0500

    expose watch code

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FODDemoKit",
-            targets: ["FODDemoKit"]),
+            targets: ["FODDemoKit", "FODDemoKitWatch"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
