5855cc07a2d93099576bb343d87874d49f117757
Amanda Beyers <amandawbeyers@gmail.com>
2023-06-07 16:37:27 -0500

    pub

--- Sources/FODDemoKitWatch/WatchConnectivityObject.swift
+++ Sources/FODDemoKitWatch/WatchConnectivityObject.swift
@@ -18,7 +18,7 @@ public class WatchConnectivityObject : ObservableObject {
     let messageDecoder = MessageDecoder(messagableTypes: [WorkoutInfo.self])
     
     @Published var workoutInfo : WorkoutInfo?
-    @Published public var messageFromPhone : String = "NO MESSAGE"
+    public @Published var messageFromPhone : String = "NO MESSAGE"
     
     public override init () {
         super.init()
